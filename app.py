012980b8b94b772ff3615cb99ec7d6c6df1693cc
Artem Volyk <AVolyk2@slb.com>
2025-11-21 10:50:55 +0400

    task

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 """Example of flask main file."""
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/api/hello')
 def hello_world():
     """Returns Hello, EDP!"""
-    return 'Hello, EDP!'
+    return f'Response received from pod: {os.getenv("HOSTNAME")}'
 
 
 if __name__ == '__main__':
